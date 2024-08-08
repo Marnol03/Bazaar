fc55df57c310958e7129cf1d56616cb0dec82915
Marnol03 <113309052+Marnol03@users.noreply.github.com>
2024-08-09 00:35:14 +0200

    Mockup

---
+++ frontend/src/comp/nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { FaBeer } from "react-icons/fa";
+import { GoListUnordered } from "react-icons/go";
+
+const Nav = () => {
+    return (
+        <>
+        <div className="header">
+            <div className="top_header">
+                <div className="logo">
+                    
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default Nav;
