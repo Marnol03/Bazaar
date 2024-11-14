91215afcced372b4e8414a06c902dbc1670cdeea
unknown <fossifranck058@gmail.com>
2024-11-14 05:32:13 +0100

    creation du profil

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 import Home from "./comp/Home"; 
-import Profil from "./comp/profil"; 
+import Profil from "./client/compte"; 
 import Connexion from "./comp/connexion";
 
 
---
+++ frontend/src/client/compte.js
@@ -0,0 +1,37 @@
+
+
+import '../css/compte.css';
+
+
+
+
+import Profil from '../comp/profil';
+
+function App() {
+  return (
+    <>
+      <Profil/>
+     
+      <Main />
+    </>  
+  );
+}
+
+
+
+
+const Main = () => {
+  return (
+    <div className='' >
+        <h1> bonjour </h1>
+    </div>
+  );
+}
+
+
+
+
+export default App;
+
+
+
