a067fe7060bed66d8b1a35030ed2701a1a06b8b4
Marnol03 <113309052+Marnol03@users.noreply.github.com>
2024-11-27 21:03:00 +0100

    database restructered, login and register logic done

--- backend/database/init.sql
+++ backend/database/init.sql
@@ -1,12 +1,13 @@
-CREATE TABLE IF NOT EXISTS products (
+CREATE TABLE IF NOT EXISTS public.products (
     id SERIAL PRIMARY KEY,
     nom VARCHAR(255),
     prix INT,
     imageUrl VARCHAR(255),
-    note DECIMAL(2, 1)
+    note DECIMAL(2, 1),
+    reduction BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE users (
+CREATE TABLE public.users (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
@@ -33,11 +34,19 @@ BEFORE UPDATE ON users
 FOR EACH ROW
 EXECUTE FUNCTION update_timestamp();
 
-INSERT INTO products (nom, prix, imageUrl, note) VALUES
-    ('Jogging', 5425, '../images/jogging.jpeg', 2.4),
-    ('Jordan nike', 12954, '../images/jordan.jpeg', 4.8),
-    ('Galaxy s22', 145250, '../images/phone_1.jpeg', 4.0),
-    ('Hp computer', 97230, '../images/laptop_1.jpg', 5.0),
-    ('Nike', 12954, '../images/nike.jpeg', 4.4),
-    ('Rolex', 733839, '../images/rolex.png', 3.7);
+INSERT INTO products (nom, prix, imageUrl, note, reduction) VALUES
+    ('Jogging', 5425, 'https://raw.githubusercontent.com/Marnol03/Bazaar/refs/heads/main/frontend/src/images/jogging.jpeg', 2.4, TRUE),
+    ('Jordan nike', 12954, 'https://raw.githubusercontent.com/Marnol03/Bazaar/refs/heads/main/frontend/src/images/jordan.jpeg', 4.8, TRUE),
+    ('Galaxy s22', 145250, 'https://raw.githubusercontent.com/Marnol03/Bazaar/refs/heads/main/frontend/src/images/phone_1.jpeg', 4.0, FALSE),
+    ('Hp computer', 97230, 'https://raw.githubusercontent.com/Marnol03/Bazaar/refs/heads/main/frontend/src/images/laptop_1.jpg', 5.0, FALSE),
+    ('Nike', 12954, 'https://raw.githubusercontent.com/Marnol03/Bazaar/refs/heads/main/frontend/src/images/nike.jpeg', 4.4, TRUE),
+    ('Rolex', 733839, 'https://raw.githubusercontent.com/Marnol03/Bazaar/refs/heads/main/frontend/src/images/rolex.png', 3.7, FALSE),
+    ('T-shirt', 2542, 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSlxtBRlMV9nF19_ieQ5A-tMCEwPIOiQgU0Tl8E9fMNVm7bu-23h7-1AVQpr_gLwGFceUJEcwEb', 3.2, FALSE),
+    ('Ecran', 3842, 'https://media.gettyimages.com/id/1370800508/de/foto/blank-pc-monitor-mockup-with-white-screen-isolated-on-white-background.jpg?s=612x612&w=gi&k=20&c=6s1KfwAFXvmlhM3pfRZFlMDDyD3H6_SwIgOgM9v7fJo=', 4.2, TRUE),
+    ('Iphone 13 pro max blue', 502954, 'https://assets.mmsrg.com/isr/166325/c1/-/ASSET_MP_108019222?x=536&y=402&format=jpg&quality=80&sp=yes&strip=yes&trim&ex=536&ey=402&align=center&resizesource&unsharp=1.5x1+0.7+0.02&cox=0&coy=0&cdx=536&cdy=402', 4.8, TRUE),
+    ('Macbook air M1', 459350, 'https://5.imimg.com/data5/SELLER/Default/2021/1/TX/FH/WC/7808107/12-inch-macbook-computer-laptops.jpg', 4.8, FALSE),
+    ('nintendo switch', 105950, 'https://assets.nintendo.eu/image/upload/f_auto,c_limit,w_1920,q_auto:low/MNS/Content%20Pages%20Assets/Category-List%20Pages/Consoles/Nintendo%20Switch%20Hub/2000x1125_Consoles_Switch_Flagship_BeautyShot', 4.8, TRUE),
+    ('Playstation 5', 320350, 'https://image.coolblue.de/422x390/products/1994458', 4.8, TRUE),
+    ('Xbox series x', 275450, 'https://www.xboxdynasty.de/wp-content/uploads/2019/12/xbox-series-x-226-720x515.jpg.pagespeed.ce.uwzoIdxBka.jpg', 4.8, TRUE);
+    
 
