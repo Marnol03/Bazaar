710e5306a6b5974026d6a31124e44a5db0e3b3b4
Marnol03 <113309052+Marnol03@users.noreply.github.com>
2024-11-27 21:02:46 +0100

    database restructered, login and register logic done

--- backend/server/index.js
+++ backend/server/index.js
@@ -11,13 +11,14 @@ const port = 5001;
 
 // Configuration pour les fichiers statiques
 const path = require('path');
+const { get } = require('http');
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use(cors());
 app.use(bodyParser.json());
 
 
-// Clé secrète pour JWT (à sécuriser dans des variables d'environnement)
+// Clé secrète pour JWT 
 const JWT_SECRET = 'dlhFgo35ijktidn7h';
 
 // Connexion à la base de données PostgreSQL
@@ -83,12 +84,11 @@ app.post('/api/register', async (req, res) => {
         // Hacher le mot de passe
         const hashedPassword = await bcrypt.hash(motDePasse, 10);
 
-        // Enregistrer l'utilisateur
         await pool.query(
             'INSERT INTO users (email, password, phone, username) VALUES ($1, $2, $3, $4)',
             [email, hashedPassword, numero, name]
         );
-
+        getuser();
         res.status(201).json({ message: 'Utilisateur enregistré avec succès.' });
     } catch (error) {
         console.error(error);
@@ -99,21 +99,24 @@ app.post('/api/register', async (req, res) => {
 // Route : Connexion d'un utilisateur
 app.post('/api/login', async (req, res) => {
     const { email, motDePasse } = req.body;
-    console.log("essai de login");
+    
     try {
         const user = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
         if (user.rows.length === 0) {
+            console.log("utilisateur non trouvé");
             return res.status(400).json({ message: 'Utilisateur non trouvé.' });
         }
 
         const isPasswordValid = await bcrypt.compare(motDePasse, user.rows[0].password);
         if (!isPasswordValid) {
+            console.log("mot de passe pareil a l\´ancien ");
             return res.status(401).json({ message: 'Mot de passe incorrect.' });
         }
 
         // Générer un token JWT
         const token = jwt.sign({ id: user.rows[0].id }, JWT_SECRET, { expiresIn: '30min' });
 
+        console.log("connexion reussie");
         res.json({ message: 'Connexion réussie.', token });
     } catch (error) {
         console.error(error);
@@ -161,14 +164,13 @@ app.post('/api/reset-password', async (req, res) => {
     const { token, newPassword } = req.body;
     console.table(req.body);
     try {
-        // Vérifier et décoder le token
         const decoded = jwt.verify(token, JWT_SECRET);
 
         const hashedPassword = await bcrypt.hash(newPassword, 10);
-        console.log("id:",decoded.id);
-        // Mettre à jour le mot de passe dans la base de données
+        
         await pool.query('UPDATE users SET password = $1 WHERE id = $2', [hashedPassword, decoded.id]);
         
+        getuser();
         res.json({ message: 'Mot de passe réinitialisé avec succès.' });
 
     } catch (error) {
@@ -178,6 +180,19 @@ app.post('/api/reset-password', async (req, res) => {
     }
 });
 
+
+// function : Récupération des utilisateurs
+async function getuser() {
+    try {
+        const result = await pool.query('SELECT * FROM users');
+        console.log("users recupérés: ", result.rows);
+    } catch (error) {
+        console.error(error);
+        console.log("Erreur lors de la récupération des utilisateurs");
+    }
+};
+
+getuser();
 app.listen(port, () => {
     console.log(`Serveur API en cours d'exécution sur http://localhost:${port}`);
 });
